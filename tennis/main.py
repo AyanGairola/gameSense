bc819a6d9dbfa7c5f041a2b5407f329ec6f0c55c
joyal-jij0 <joyal2405@gmail.com>
2024-08-31 14:33:55 +0530

    feat: Add Ball tracking for Tennis

--- tennis/main.py
+++ tennis/main.py
@@ -1,5 +1,6 @@
 from utils import (read_video, save_video)
 from trackers import PlayerTracker
+from trackers import BallTracker
 
 def main():
 
@@ -7,17 +8,24 @@ def main():
     input_video_path = "input_vods/input_video1.mp4"
     video_frames = read_video(input_video_path)
 
-    #Detect Players
+    #Detect Players and Ball
     player_tracker = PlayerTracker(model_path='yolov8x')
+    ball_tracker = BallTracker(model_path='models/yolo5_last.pt')
+
+
     player_detections = player_tracker.detect_frames(video_frames,read_from_stub=True,stub_path="tracker_stubs/player_detections.pkl")
+
+    ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl")
+    
 
     #Draw Output
 
     ##Draw Player Bouding Boxes
     output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames= ball_tracker.draw_bboxes(video_frames, ball_detections)
 
 
-    save_video(video_frames, "output_vods/output_video.mp4")
+    save_video(output_video_frames, "output_vods/output_video.mp4")
 
 if __name__ == "__main__":
     main()
